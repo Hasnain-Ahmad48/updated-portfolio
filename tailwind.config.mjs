ee02750a51526a0927b6f6e534c06e85a1dab20b
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-12-19 22:12:54 +0500

    navbar added with some style

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+import { Outfit } from "next/font/google";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default{
+    content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors:{
+                lightHover:"#fcf4ff",
+                darkHover:'#2a004a',
+                darkTheme:'#11001f',
+            },
+            fontFamily:{
+                Outfit:['Outfit', 'sans-serif'], 
+                Ovo:['Ovo', 'serif'],
+            }
+        },
+    },
+    plugins: [],
+}
