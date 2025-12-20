61cc5d475b103695e1b07cb32f29461e14ce6780
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-12-20 14:33:59 +0500

    added nav bar for mobile and desktop screen

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import {assets} from "@/assets/assets";
 import Image from "next/image";
-import React from "react";
+import React, {useRef} from "react";
 
 const Navbar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(-16rem)";
+  };
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = "translateX(16rem)";
+  };
+
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -84,7 +93,10 @@ const Navbar = () => {
             />
           </a>
 
-          <button className="block md:hidden ml-3">
+          <button
+            className="block md:hidden ml-3"
+            onClick={openMenu}
+          >
             <Image
               src={assets.menu_black}
               alt=""
@@ -92,6 +104,70 @@ const Navbar = () => {
             />
           </button>
         </div>
+
+        {/* -- mobile menu */}
+
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div
+            className="absolute right-6 top-6"
+            onClick={closeMenu}
+          >
+            <Image
+              src={assets.close_black}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          </div>
+
+          <li>
+            <a
+              className="font-medium"
+              onClick={closeMenu}
+              href="#top"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-medium"
+              onClick={closeMenu}
+              href="#about"
+            >
+              About Me
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-medium"
+              onClick={closeMenu}
+              href="#services"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-medium"
+              onClick={closeMenu}
+              href="#work"
+            >
+              My Work
+            </a>
+          </li>
+          <li>
+            <a
+              className="font-medium"
+              onClick={closeMenu}
+              href="#contact"
+            >
+              Contact Me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
