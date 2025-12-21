3ab9012ea7a3d60f32d8cdc20802c690229a4460
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-12-21 21:46:40 +0500

    install tailwind v3

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -8,9 +8,7 @@ const About = () => {
       id="about"
       className="w-full px-[12%] py-10 scroll-mt-20"
     >
-      <h2 className="bg-darkTheme text-white shadow-white font-outfit">
-        Tailwind Custom Working
-      </h2>
+      
       <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
       <h2 className="text-center text-5xl font-Ovo">About me</h2>
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
