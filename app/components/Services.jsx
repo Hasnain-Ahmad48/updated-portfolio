4a3ef1427bd1d83646a6085a92b5a1eb96fbda5c
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-12-23 15:52:10 +0500

    dark mode for services section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -3,7 +3,7 @@ import {Asset} from "next/font/google";
 import Image from "next/image";
 import React from "react";
 
-const Services = () => {
+const Services = ({isDarkMode}) => {
   return (
     <div>
       <div
@@ -18,15 +18,21 @@ const Services = () => {
         </p>
         <div className="grid grid-cols-auto gap-6 my-10">
           {serviceData.map(({icon, title, description, link}, index) => (
-            <div key={index}
-            className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:translate-y-1 duration-500">
+            <div
+              key={index}
+              className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white"
+            >
               <Image
                 src={icon}
                 alt=""
                 className="w-10"
               />
-              <h3 className="text-lg my-4 text-gray-700">{title}</h3>
-              <p className="text-sm text-gray-600 leading-5">{description}</p>
+              <h3 className="text-lg my-4 text-gray-700 dark:text-white">
+                {title}
+              </h3>
+              <p className="text-sm text-gray-600 leading-5 dark:text-white/80">
+                {description}
+              </p>
               <a
                 href={link}
                 className="flex items-center gap-2 text-sm mt-5"
@@ -34,7 +40,11 @@ const Services = () => {
                 Read more{" "}
                 <Image
                   alt=""
-                  src={assets.right_arrow}
+                  src={
+                    isDarkMode
+                      ? assets.right_arrow_bold_dark
+                      : assets.right_arrow
+                  }
                   className="w-4 "
                 />
               </a>
