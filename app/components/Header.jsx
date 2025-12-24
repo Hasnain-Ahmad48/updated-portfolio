4994928f0a48c9bfd53ca8b805c9ad0a17e8b17a
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-12-24 16:15:21 +0500

    dark theme for footer and contact page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,17 +1,22 @@
 import {assets} from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import {motion} from "motion/react";
 
 const Header = () => {
   return (
     <div className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4">
-      <div>
+      <motion.div
+        initial={{scale: 0}}
+        whileInView={{scale: 1}}
+        transition={{duration: 0.8, type: "spring", stiffness: 100}}
+      >
         <Image
           src={assets.profile_img}
           alt=""
           className="rounded-full w-32"
         />
-      </div>
+      </motion.div>
       <h3 className="flex items-end gap-2 text-xl md:text-2xl md-3 font-medium">
         Hi! I am Hasnain Ahmad{" "}
         <Image
@@ -28,9 +33,10 @@ const Header = () => {
         responsive and user-friendly web applications.
       </p>
       <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
-        <a href="#contact" 
-        className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
-        >  
+        <a
+          href="#contact"
+          className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent"
+        >
           Contact Me
           <Image
             src={assets.right_arrow_white}
